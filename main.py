b2d9b7849eed8b258738f4b305b986e9608e125f
Adrian-Luerssen <91116739+Adrian-Luerssen@users.noreply.github.com>
2023-03-09 21:31:57 +0100

    very simple NLP processing with responses defined in dictionary

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ Created on Wed Mar  8 23:25:39 2023
 
 @author: adril
 """
-
+import random as r
 import nltk
-# from nltk.stem import PorterStemmer, LancasterStemmer, WordNetLemmatizer, SnowballStemmer
+from nltk.stem import PorterStemmer, LancasterStemmer, WordNetLemmatizer, SnowballStemmer
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 
@@ -38,8 +38,13 @@ def preProcessing (string):
     tokenizer = RegexpTokenizer(r'\w+')
     tokens = tokenizer.tokenize(string)
     tokens = [w for w in tokens if not w.lower() in stop_words]
+    #lancaster = LancasterStemmer()
+    stemmed=[]
+    for w in tokens:
+        #stemmed.append(lancaster.stem(w))
+        stemmed.append(w.lower())
             
-    return tokens
+    return stemmed
 
 def updateVocab ():
     vectorizer = sklearn.feature_extraction.text.CountVectorizer()
@@ -53,27 +58,43 @@ def updateVocab ():
     return vectorizer
         
     
+def generateResponse(sentence):
+    tokens = preProcessing(sentence)
+    responses = {'add':'i will add this task to your schedule ',
+                 'cancel':'this event has been canceled ',
+                 'show':'Here are the events you have',
+                 'schedule':'i have put this in your calendar ',
+                 'reschedule':'i have updated this event',
+                 'update':'i have updated your calendar '}
     
+    response = 'i dont know how to respond'
+    #print(tokens)
+    if tokens[0] in responses:
+        response = responses[tokens[0]]
+    return response
     
-string = "hello how are you?"#input("enter a sentence: ")
-tokens = preProcessing(string)
-print (tokens)
-    
+
 string = "Can you schedule an event for tomorrow at 2pm"#input("enter a sentence: ")
 tokens = preProcessing(string)
-print (tokens)
+#print (tokens)
     
 string = "when is the next 2h gap i have?"#input("enter a sentence: ")
 tokens = preProcessing(string)
-print (tokens)
+#print (tokens)
 
 with open("data/testPhrases.txt") as f:
     lines = [line.rstrip() for line in f]
+limit = 5
 for line in lines:
-    print(preProcessing(line))
+    if (limit >= 1 and r.randint(0,100) > 60):
+        print("in:  ",line)
+        print("out: ",generateResponse(line))
+        print()
+        limit-=1;
 #print(pd.get_dummies(tokens))
 
 
-print(updateVocab().vocabulary_)
+#print(updateVocab().vocabulary_)
 
-
+#sting = input ("please type a sentence: ")
+#print ("chatbot: "+generateResponse(sting))
