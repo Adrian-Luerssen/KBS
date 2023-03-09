f81e5d3f6bf55d816e4ee03b5aff368fbce34ff8
Adrian-Luerssen <91116739+Adrian-Luerssen@users.noreply.github.com>
2023-03-09 17:29:11 +0100

    preprocessing

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Created on Wed Mar  8 23:25:39 2023
 
 import nltk
 # from nltk.stem import PorterStemmer, LancasterStemmer, WordNetLemmatizer, SnowballStemmer
-# from nltk.corpus import stopwords
+from nltk.corpus import stopwords
 
 import pandas as pd
 
@@ -28,16 +28,22 @@ from sklearn.feature_extraction.text import CountVectorizer
 # import scipy
 
 #nltk.download('punktl')
+#nltk.download('stopwords')
+#nltk.download('wordnet')
 
 
-def tokenize (string):
-    return nltk.word_tokenize(string)
+def preProcessing (string):
+    stop_words = set(stopwords.words('english'))
+    tokens = nltk.word_tokenize(string)
+    tokens = [w for w in tokens if not w.lower() in stop_words]
+            
+    return tokens
 
 def updateVocab ():
     vectorizer = sklearn.feature_extraction.text.CountVectorizer()
     sentences = []
     with open("data/testPhrases.txt") as file:
-        sentences.append(tokenize(file.read()))
+        sentences.append(file.read())
         
     vectorizer.fit(sentences)
     return vectorizer
@@ -45,13 +51,21 @@ def updateVocab ():
     
     
     
-string = "hello how are you? 7pm"#input("enter a sentence: ")
-tokens = tokenize(string)
+string = "hello how are you?"#input("enter a sentence: ")
+tokens = preProcessing(string)
+print (tokens)
+    
+string = "Can you schedule an event for tomorrow at 2pm"#input("enter a sentence: ")
+tokens = preProcessing(string)
+print (tokens)
+    
+string = "when is the next 2h gap i have?"#input("enter a sentence: ")
+tokens = preProcessing(string)
 print (tokens)
 
 #print(pd.get_dummies(tokens))
 
 
-print(updateVocab().vocabulary_)
+#print(updateVocab().vocabulary_)
 
 
